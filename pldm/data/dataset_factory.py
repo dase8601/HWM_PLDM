4a62910476a1711725ff949a560d60e8da6019f4
dase8601 <dase8601@colorado.edu>
2026-05-03 13:42:42 -0500

    Update dataset_factory.py

--- pldm/data/dataset_factory.py
+++ pldm/data/dataset_factory.py
@@ -1,10 +1,14 @@
 import dataclasses
 
 from pldm.data.utils import make_dataloader
+
+
+# if "AMD" not in torch.cuda.get_device_name(0):
 from pldm_envs.diverse_maze.d4rl import D4RLDataset
 
 from pldm.probing.evaluator import ProbingConfig
 from pldm.data.enums import DataConfig, DatasetType, ProbingDatasets, Datasets
+
 
 
 class DatasetFactory:
@@ -25,19 +29,29 @@ class DatasetFactory:
         self.aae_samples = aae_samples
 
     def create_datasets(self):
-        if self.config.dataset_type == DatasetType.D4RL:
+        if self.config.dataset_type == DatasetType.Single:
+            return self._create_single_datasets()
+        elif self.config.dataset_type == DatasetType.D4RL:
             return self._create_d4rl_datasets()
+        elif self.config.dataset_type == DatasetType.LocoMaze:
+            return self._create_locomaze_datasets()
         else:
-            raise NotImplementedError(
-                f"Unsupported dataset type: {self.config.dataset_type}. "
-                "Only D4RL is supported in this run."
-            )
+            raise NotImplementedError
+
+    def _create_single_datasets(self):
+        ds = DotDataset(self.config.dot_config)
+        val_ds = DotDataset(
+            dataclasses.replace(self.config.dot_config, train=False),
+            normalizer=ds.normalizer,
+        )
+
+        datasets = Datasets(ds=ds, val_ds=val_ds)
+
+        return datasets
+
 
     def _create_d4rl_datasets(self):
-        ds = D4RLDataset(
-            self.config.d4rl_config,
-            load_l1=self.config.d4rl_config.train_l1,
-        )
+        ds = D4RLDataset(self.config.d4rl_config, load_l1=self.config.d4rl_config.train_l1)
         ds = make_dataloader(ds=ds, loader_config=self.config)
 
         probe_ds = D4RLDataset(
@@ -96,7 +110,6 @@ class DatasetFactory:
                 normalizer=ds.normalizer,
                 suffix="l2_probe_train",
             )
-
             l2_probe_val_ds = D4RLDataset(
                 dataclasses.replace(
                     self.config.d4rl_config,
@@ -115,7 +128,6 @@ class DatasetFactory:
                 normalizer=ds.normalizer,
                 suffix="l2_probe_val",
             )
-
             datasets = Datasets(
                 ds=ds,
                 val_ds=None,
@@ -124,5 +136,88 @@ class DatasetFactory:
                     ds=l2_probe_ds, val_ds=l2_probe_val_ds
                 ),
             )
+
+        return datasets
+
+    def _create_locomaze_datasets(self):
+        # TODO unify with _create_d4rl_datasets?
+        ds = LocoMazeDataset(self.config.d4rl_config)
+        ds = make_dataloader(ds=ds, loader_config=self.config)
+
+        val_ds = None
+        if self.config.d4rl_config.val_path is not None:
+            val_ds = LocoMazeDataset(
+                dataclasses.replace(
+                    self.config.d4rl_config,
+                    path=self.config.d4rl_config.val_path,
+                    train=False,
+                )
+            )
+
+            val_ds = make_dataloader(
+                ds=val_ds,
+                loader_config=self.config,
+                normalizer=ds.normalizer,
+                suffix="val",
+            )
+
+        probe_ds = LocoMazeDataset(
+            dataclasses.replace(
+                self.config.d4rl_config,
+                path=self.probing_cfg.train_path,
+                n_steps=self.probing_cfg.l1_depth,
+            ),
+        )
+        probe_ds = make_dataloader(
+            ds=probe_ds,
+            loader_config=self.config,
+            normalizer=ds.normalizer,
+            suffix="probe_train",
+        )
+
+        probe_val_ds = LocoMazeDataset(
+            dataclasses.replace(
+                self.config.d4rl_config,
+                path=self.probing_cfg.val_path,
+                n_steps=self.probing_cfg.l1_depth,
+                train=False,
+                crop_length=50000,
+                batch_size=64,
+                load_top_down_view=self.probing_cfg.visualize_probing,
+            ),
+        )
+        probe_val_ds = make_dataloader(
+            ds=probe_val_ds,
+            loader_config=self.config,
+            normalizer=ds.normalizer,
+            suffix="probe_val",
+        )
+
+        aae_ds = None
+        if self.eval_aae:
+            aae_ds = LocoMazeDataset(
+                dataclasses.replace(
+                    self.config.d4rl_config,
+                    path=self.config.d4rl_config.val_path,
+                    n_steps=self.aae_chunk_size + 1,
+                    train=False,
+                    crop_length=self.aae_samples,
+                    batch_size=64,
+                    load_top_down_view=True,
+                ),
+            )
+            aae_ds = make_dataloader(
+                ds=aae_ds,
+                loader_config=self.config,
+                normalizer=ds.normalizer,
+                suffix="aae",
+            )
+
+        datasets = Datasets(
+            ds=ds,
+            val_ds=val_ds,
+            probing_datasets=ProbingDatasets(ds=probe_ds, val_ds=probe_val_ds),
+            aae_dataset=aae_ds,
+        )
 
         return datasets
